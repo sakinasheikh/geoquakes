dcafc5824a743f2f53c69403b5a984ac53b8e498
nathanallen <thenathanator@gmail.com>
2015-08-19 09:01:48 -0700

    solution using underscore template

--- base.js
+++ base.js
@@ -10,11 +10,15 @@ $.get(weekly_quakes_endpoint, function(response) {
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 
+    // UNDERSCORE: pre-compile info row template
+    var info_row_html = $("#tmpl-info-row").html();
+    var compiledInfoRow = _.template(info_row_html)
+
     // LOOP OVER EARTHQUAKES
     response.features.forEach(function(quake){
 
         var title = quake.properties.title;
-        var hours_ago = Math.round( ( Date.now() - quake.properties.time ) / (1000*60*60) )
+        var hours_ago = Math.round( ( Date.now() - quake.properties.time ) / (1000*60*60) );
 
         // ADD QUAKE MARKER TO MAP
         var lat = quake.geometry.coordinates[1];
@@ -26,11 +30,8 @@ $.get(weekly_quakes_endpoint, function(response) {
         });
 
         // ADD QUAKE INFO TO COLUMN
-        $("#info").append(
-                            "<p>" +
-                                title + " / " + hours_ago + " hours ago " +
-                            "</p>"
-                          )
+        var info_row_html = compiledInfoRow({title: title, hours_ago: hours_ago})
+        $("#info").append( info_row_html );
     });
 
 });
