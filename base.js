62aa652b974f637725d3b1fa670a97d4b3bc6eb8
nathanallen <thenathanator@gmail.com>
2015-08-19 08:44:21 -0700

    basic solution

--- base.js
+++ base.js
@@ -1,1 +1,36 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
+
+$.get(weekly_quakes_endpoint, function(response) {
+
+    var mapOptions = {
+      center: { lat: -34.397, lng: 150.644},
+      zoom: 1
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+    // LOOP OVER EARTHQUAKES
+    response.features.forEach(function(quake){
+
+        var title = quake.properties.title;
+        var hours_ago = Math.round( ( Date.now() - quake.properties.time ) / (1000*60*60) )
+
+        // ADD QUAKE MARKER TO MAP
+        var lat = quake.geometry.coordinates[1];
+        var lng = quake.geometry.coordinates[0];
+        new google.maps.Marker({
+            position: new google.maps.LatLng(lat, lng),
+            map: map,
+            title: title
+        });
+
+        // ADD QUAKE INFO TO COLUMN
+        $("#info").append(
+                            "<p>" +
+                                title + " / " + hours_ago + " hours ago " +
+                            "</p>"
+                          )
+    });
+
+});
